9ed28cbf342a170ddb55b36a30029000887d5285
Sophie <thanjusophie@gmail.com>
2025-08-06 11:11:02 -0400

    Initial Commit

---
+++ GIT_Practice/GIT_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_PracticeApp.swift
+//  GIT_Practice
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GIT_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
